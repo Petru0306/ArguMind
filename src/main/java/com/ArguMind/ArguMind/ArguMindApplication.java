b2f205ee66550c75c784cdf354a6f704cae82566
Petru0306 <petru.panait77@gmail.com>
2026-05-21 13:38:10 +0300

    first commit

---
+++ src/main/java/com/ArguMind/ArguMind/ArguMindApplication.java
@@ -0,0 +1,13 @@
+package com.ArguMind.ArguMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArguMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArguMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArguMind/ArguMind/ArguMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArguMind.ArguMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArguMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
